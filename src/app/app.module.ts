b4d69b0bd4fc13babd160d310f389b4363a2fc2a
Guest User <Guest@Epicodus-4D.local>
2018-04-10 11:55:40 -0700

    set up room to loop through rooms and display current room

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
 import { FrontPageComponent } from './front-page/front-page.component';
-import { Room000Component } from './room000/room000.component';
 import { LogComponent } from './log/log.component';
 import { PlayerComponent } from './player/player.component';
 import { TestDatabaseComponent } from './test-database/test-database.component';
+import { Room000Component } from './room000/room000.component';
 
 
 export const firebaseConfig = {
@@ -24,10 +24,11 @@ export const firebaseConfig = {
   declarations: [
     AppComponent,
     FrontPageComponent,
-    Room000Component,
     LogComponent,
     PlayerComponent,
-    TestDatabaseComponent
+    TestDatabaseComponent,
+    Room000Component,
+  
   ],
   imports: [
     BrowserModule,
--- src/app/room000/room000.component.ts
+++ src/app/room000/room000.component.ts
@@ -9,12 +9,33 @@ import { RoomService } from '../services/room.service';
   providers: [RoomService]
 })
 export class Room000Component implements OnInit {
+  rooms;
+  scenes;
+  currentRoom;
+  currentScene;
+  move = false;
 
-
-  constructor() { }
+  constructor(private roomService : RoomService) { }
 
   ngOnInit() {
+    this.roomService.getRooms().subscribe(rooms=>{
+      this.rooms = rooms
+      this.rooms.forEach(function(room){
+        console.log(room.scene)
+        })
+      // console.log(this.rooms)
+      this.currentRoom = "Recovery";
+      this.currentScene
+    })
+  }
 
+  changeRoom(id){
+    this.roomId = id;
+    console.log(id);
+  }
+
+  moveOptions(){
+    this.move = true;
   }
 
 
