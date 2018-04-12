baefcc125e3c5489b3048740f752d8aeef81923e
Hamza Naeem and Sara Hamilton <sara@>
2018-04-12 12:42:11 -0700

    add resetScenesInDatabase method to log component

--- src/app/log/log.component.ts
+++ src/app/log/log.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { RoomService } from '../services/room.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-log',
   templateUrl: './log.component.html',
-  styleUrls: ['./log.component.css']
+  styleUrls: ['./log.component.css'],
+  providers: [RoomService]
 })
 export class LogComponent implements OnInit {
-
-  constructor() { }
+  scenes;
+  constructor(private roomService : RoomService) { }
 
   ngOnInit() {
+    this.roomService.getScenes().subscribe(scenes => {
+      this.scenes = scenes;
+    });
+  }
+
+  resetScenesInDatabase() {
+    for (var i = 0; i < 30; i++) {
+      this.roomService.updateScene(this.scenes[i].id, 0)
+    }
   }
 
 }
