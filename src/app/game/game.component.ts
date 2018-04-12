416ca36e904ce9ad3501d1dbf45e4da839a8fcdb
Hamza Naeem and Sara Hamilton <sara@>
2018-04-12 12:47:08 -0700

    remove unused code

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,28 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { RoomService } from '../services/room.service';
-import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css'],
-  providers: [RoomService]
+  styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-  scenes;
 
-  constructor(private roomService : RoomService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.roomService.getScenes().subscribe(scenes => {
-      this.scenes = scenes;
-    });
   }
-
-  // resetScenesInDatabase() {
-  //   this.scenes.forEach(function(scene) {
-  //     this.roomService.updateScene(scene.id, 0);
-  //   });
-  // }
 
 }
